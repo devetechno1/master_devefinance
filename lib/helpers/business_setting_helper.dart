ea9ddd5b14ec6bbeaaa2eee6ef3cee8e0f6ecb07
Mohammed Riyad <riyadm2001@gmail.com>
2025-04-08 11:54:16 +0200

    fix init lang

--- lib/helpers/business_setting_helper.dart
+++ lib/helpers/business_setting_helper.dart
@@ -2,6 +2,10 @@ import 'package:active_ecommerce_cms_demo_app/data_model/business_setting_respon
 import 'package:active_ecommerce_cms_demo_app/helpers/shared_value_helper.dart';
 import 'package:active_ecommerce_cms_demo_app/repositories/business_setting_repository.dart';
 
+import '../app_config.dart';
+import '../data_model/language_list_response.dart';
+import '../repositories/language_repository.dart';
+
 class BusinessSettingHelper {
   setBusinessSettingData() async {
     BusinessSettingListResponse businessLists =
@@ -199,5 +203,39 @@ class BusinessSettingHelper {
           break;
       }
     });
+  }
+
+  static Future<void> setInitLang()async {
+    final LanguageListResponse langs = await LanguageRepository().getLanguageList();
+
+    if (langs.success != true) return;
+
+    if(langs.languages?.length == 1){
+      await _setLangConfig(langs.languages!.first);
+      return;
+    }
+    await app_language.load();
+    if(app_language.$ == null){
+      for (Language lang in langs.languages ?? []) {
+        if (lang.is_default ?? false) {
+          await _setLangConfig(lang);
+          return;
+        }
+      }
+    }
+  }
+
+  static Future<void> _setLangConfig (Language lang) async{
+    AppConfig.default_language = lang.code ?? AppConfig.default_language;
+    AppConfig.mobile_app_code = lang.mobile_app_code ?? AppConfig.mobile_app_code;
+    AppConfig.app_language_rtl = lang.rtl ?? AppConfig.app_language_rtl;
+    app_language.$ = lang.code;
+    app_mobile_language.$ = lang.mobile_app_code;
+    app_language_rtl.$ = lang.rtl;
+    await Future.wait([
+      app_language.save(),
+      app_mobile_language.save(),
+      app_language_rtl.save(),
+    ]);
   }
 }
--- lib/providers/locale_provider.dart
+++ lib/providers/locale_provider.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:active_ecommerce_cms_demo_app/helpers/shared_value_helper.dart';
 
+import '../app_config.dart';
+
 class LocaleProvider with ChangeNotifier {
   Locale? _locale;
   Locale get locale {
     //print("app_mobile_language.isEmpty${app_mobile_language.$.isEmpty}");
     return _locale =
-        Locale(app_mobile_language.$ == '' ? "en" : app_mobile_language.$!, '');
+        Locale(app_mobile_language.$ == '' ? AppConfig.mobile_app_code : app_mobile_language.$!, '');
   }
 
   void setLocale(String code) {
