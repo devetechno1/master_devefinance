895f5d9ff0f9cc6ae636e60d2ea9be1b35e8d4a6
Mohammed Riyad <riyadm2001@gmail.com>
2025-04-16 12:12:59 +0200

    make executeAndHandleErrors

---
+++ lib/status/execute_and_handle_remote_errors.dart
@@ -0,0 +1,23 @@
+
+import 'status.dart';
+
+Future<Status<T>> executeAndHandleErrors<T>(
+  Future<T> Function() function, [
+  Future<T?> Function()? functionWhenError,
+]) async {
+  try {
+    return Success<T>(await function());
+  } catch (e) {
+    print("error: $e");
+
+    T? data;
+    if (functionWhenError != null) data = await functionWhenError();
+
+    // if (e is DioException) {
+    //   if (AppInfo.isDebugMode) print("DioException: ${e.response}");
+    //   return ServerFailure<T>.fromDioException(e).copyWith(data: data);
+    // }
+
+    return Failure<T>(FailureBody(message: e.toString()), data);
+  }
+}
---
+++ lib/status/status.dart
@@ -0,0 +1,11 @@
+export 'errors/failure.dart';
+export 'success/success.dart';
+
+abstract class Status<T> {
+  const Status();
+}
+
+// class Loading extends Status {
+//   const Loading([this.loadingMore = false]);
+//   final bool loadingMore;
+// }
