c2b181a8fb655cabde4c1325bce78169d90868ff
Your Name <you@example.com>
2025-04-20 15:06:13 +0200

    make stringToColor fn to get it from api

---
+++ test/unit_test.dart
@@ -0,0 +1,18 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:active_ecommerce_cms_demo_app/helpers/color_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Test color converter', () {
+    final Color? color = ColorHelper.stringToColor("050");
+
+    expect(color, Color(0xFFFFF050));
+  });
+}
