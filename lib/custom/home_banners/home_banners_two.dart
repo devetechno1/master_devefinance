6a2826dbcb6ee377c660bc493c39b068877be7df
Your Name <you@example.com>
2025-11-24 17:03:53 +0200

    Improve home banner type definitions, add vertical padding to banner two, and update meta and test_api dependencies.

--- lib/custom/home_banners/home_banners_two.dart
+++ lib/custom/home_banners/home_banners_two.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 
+import 'package:active_ecommerce_cms_demo_app/constants/app_dimensions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,16 +13,23 @@ class HomeBannersTwo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final p = context.select<HomeProvider,
-        ({bool isBannerTwoInitial, UnmodifiableListView<AIZSlider> bannerTwoImageList})>(
+    final p = context.select<
+        HomeProvider,
+        ({
+          bool isBannerTwoInitial,
+          UnmodifiableListView<AIZSlider> bannerTwoImageList
+        })>(
       (provider) => (
         bannerTwoImageList: UnmodifiableListView(provider.bannerTwoImageList),
         isBannerTwoInitial: provider.isBannerTwoInitial,
       ),
     );
-    return HomeBannersList(
-      bannersImagesList: p.bannerTwoImageList,
-      isBannersInitial: p.isBannerTwoInitial,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: AppDimensions.paddingSmall),
+      child: HomeBannersList(
+        bannersImagesList: p.bannerTwoImageList,
+        isBannersInitial: p.isBannerTwoInitial,
+      ),
     );
   }
 }
